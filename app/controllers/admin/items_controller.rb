676a725f74f3ed8fbf1f9c766877a15ff399bd90
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-06-07 22:58:30 +0000

    making admin/items/new

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,24 @@
 class Admin::ItemsController < ApplicationController
-  def new
-  end
-
-  def index
-  end
-
-  def show
-  end
-
-  def edit
-  end
+    def new
+      @item = Item.new
+    end
+    
+    # def create
+    #   @item = Item.new
+    # end
+  
+    def index
+    end
+  
+    def show
+    end
+  
+    def edit
+    end
+  
+  private
+    
+    # def item_params
+    #   params.require(:item).permit()
+    # end
 end
