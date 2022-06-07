48070faf2a9916d2dbec24bb7c3f8819b667e2c2
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-06-07 13:29:49 +0000

    making admin/log_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :items, only:[:new, :index, :show, :edit, :create, :update]
+  end
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
