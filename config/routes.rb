3bf9d8978e172498a52b1b8ccd53d5dea2a02441
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-06-11 13:36:57 +0000

    making public/homes/top routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 Rails.application.routes.draw do
+
+  namespace :public do
+    root to: 'homes#top'
+  end
   namespace :admin do
+    get '/' => 'homes#top'
     resources :items, only:[:new, :create, :index, :show, :edit, :update]
     resources :genres, only:[:index, :create, :edit, :update]
   end
 
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
-}
+  }
+
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
