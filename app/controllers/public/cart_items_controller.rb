98c341e4155f28a36a8fd0982640ddf1f5409b12
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-06-16 03:36:15 +0000

    making index

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,11 +1,18 @@
 class Public::CartItemsController < ApplicationController
   def index
+    
   end
 
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
     @cart_item.save
+    redirect_to public_cart_items_path
+  end
+
+  def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
     redirect_to public_cart_items_path
   end
 
