0604106c0c9051de4e40cd6fcef419f4433e515a
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-06-18 04:42:53 +0000

    making public/cart_items

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,9 +5,15 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    @cart_item.save
-    redirect_to public_cart_items_path
+    if @cart_item == CartItem.find_by(@item.name)
+      @cart_item.amount += params[:cart_item][:amount].to_i
+      @cart_item.save
+      redirect_to public_cart_items_path
+    else
+      @cart_item.customer_id = current_customer.id
+      @cart_item.save
+      redirect_to public_cart_items_path
+    end
   end
 
   def update
