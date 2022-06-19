c5d5b1bd3fa863dd02e6ff5b8c360823bfcb0b66
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-06-19 04:46:03 +0000

    making cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,15 +5,13 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item = CartItem.new(cart_item_params)
-    if CartItem.find_by(params[:item_id]).present?
-      @cart_item = CartItem.find_by(params[:item_id])
-      @cart_item.amount += params[:amount].to_i
-      binding.pry
-      @cart_item.save
+    @cart_item.customer_id = current_customer.id
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      add_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      add_item.amount += params[:cart_item][:amount].to_i
+	    add_item.save
       redirect_to public_cart_items_path
     else
-      @cart_item.customer_id = current_customer.id
-    binding.pry
       @cart_item.save
       redirect_to public_cart_items_path
     end
