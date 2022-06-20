fd365930ae096d68bb1a452de6fcd1a1b7085df4
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-06-20 09:09:34 +0000

    add admin account

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -11,11 +11,10 @@ class Public::CartItemsController < ApplicationController
       add_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
       add_item.amount += params[:cart_item][:amount].to_i
 	    add_item.save
-      redirect_to public_cart_items_path
     else
       @cart_item.save
-      redirect_to public_cart_items_path
     end
+      redirect_to public_cart_items_path
   end
 
   def update
