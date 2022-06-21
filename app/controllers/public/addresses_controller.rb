737160e8a82e4c5d491b44142571ba2f9a18b36d
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-06-21 06:37:45 +0000

    finish public/address

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,34 @@
 class Public::AddressesController < ApplicationController
   def index
-    @customer = Customer.find(current_customer.id)
-    
-    # @addresses = Address.find(current_customer.id)
+    @address = Address.new
+    @addresses = Address.all
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to public_addresses_path
   end
 
   def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to public_addresses_path
+  end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @address.delete
+    redirect_to public_addresses_path
   end
 
 private
 
   def address_params
-    params.require(:address).permit(:name, :postal_code, :address)
+    params.require(:address).permit(:name, :postal_code, :address, :customer_id)
   end
 end
