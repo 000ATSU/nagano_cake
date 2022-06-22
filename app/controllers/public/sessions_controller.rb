2e47de5ac5e4948b8f9ae48cc00565e0e1ba0a27
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-06-22 04:49:33 +0000

    finish customer_state

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -36,12 +36,10 @@ protected
 
   def customer_state
     @customer = Customer.find_by(email: params[:customer][:email])
-    ## アカウントを取得できなかった場合、このメソッドを終了する
     return if !@customer
-    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
-    if @customer.valid_password?(params[:customer][:password])
-    ## 【処理内容3】
+    if @customer.valid_password?(params[:customer][:password]) && !@customer.is_active
+      redirect_to new_customer_registration_path
     end
   end
+
 end
-  
