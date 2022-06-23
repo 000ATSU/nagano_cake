9d2f58fe583c699c8a5f2970793a89b35f4dab46
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-06-23 08:02:43 +0000

    making public/orders/new

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,6 +1,9 @@
 class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
+    @order.shipping_zip_code = current_customer.postal_code
+    @order.shipping_address = current_customer.address
+    @order.delivery_name = current_customer.first_name + current_customer.last_name
   end
 
   def confirmation
@@ -13,5 +16,11 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+
+private
+
+  def order_params
+  params.require(:order).permit(:method_of_payment, :selected_address, :a)
   end
 end
