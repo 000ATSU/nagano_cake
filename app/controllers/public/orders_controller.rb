4ca0f5df73fb2a927fa2e2c66ca91154a1d678aa
000ATSU <99629901+000ATSU@users.noreply.github.com>
2022-06-23 23:15:44 +0000

    making public/orders/confirmation controller

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,6 +8,18 @@ class Public::OrdersController < ApplicationController
 
   def confirmation
     @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    if params[:order][:selected_address] == 1
+      @address = Address.find(params[:order][:address_id])
+      @order.shipping_zip_code = @address.postal_code
+      @order.shipping_address = @address.address
+      @order.delivery_name = @address.name
+    elsif params[:order][:selected_address] == 2
+      @order = Order.new(order_params)
+      @order.shipping_zip_code = current_customer.postal_code
+      @order.shipping_address = current_customer.address
+      @order.delivery_name = current_customer.first_name + current_customer.last_name
+    end
     binding.pry
   end
 
@@ -23,6 +35,6 @@ class Public::OrdersController < ApplicationController
 private
 
   def order_params
-    params.require(:order).permit(:method_of_payment)
+    params.require(:order).permit(:method_of_payment, :shipping_zip_code, :shipping_address, :delivery_name)
   end
 end
